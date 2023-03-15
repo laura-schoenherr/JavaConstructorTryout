45306cc4fcd68398ab50e07bfa5295635fe63900
laura-schoenherr <lauraschoenherr@outlook.de>
2023-03-15 22:07:45 +0100

    initialise shopping class with standard constructor and parameter constructor

---
+++ src/shopping.java
@@ -0,0 +1,21 @@
+public class shopping {
+    //unveränderliche Instanzvariablen
+    float weight;
+    float price;
+
+    //Standardkonstruktor
+    /* shopping(){
+        System.out.println("Dies ist der Standardkonstruktor");
+    }
+        */
+    //Initialisierungskonstruktor - parametrisierter Konstruktor (ähnlich Setter methode)
+    shopping(float weightvalue, float pricevalue){
+        weight = weightvalue;
+        price = pricevalue;
+    }
+
+    //test method to show values from the object
+    void getValues(){
+        System.out.println("Weight: " + weight + "\n" + "Price: " + price);
+    }
+}
---
+++ src/testtreibermain.java
@@ -0,0 +1,13 @@
+public class testtreibermain {
+
+    public static void main (String[] args) {
+        // tryout shopping Standardkonstruktor
+        // shopping tryout = new shopping();
+        // tryout.getValues();
+
+        //tryout Initialisierungskonstruktor
+        shopping tryout = new shopping(7, 17);
+        tryout.getValues();
+    }
+
+}
